944e1a53204a516469ffbc7869eaf46d9f064fbb
burntcustard <burntcustard@gmail.com>
2019-08-16 08:42:29 +0100

    'Probably' picked a way to store the ship shape/model data

--- src/js/ships/tri.js
+++ src/js/ships/tri.js
@@ -1,22 +1,13 @@
 
 export default () => {
     return {
-        rof: 8,
-        model: [
-            [ -3, -5 ],
-            [ 12,  0 ],
-            [ -3,  5 ]
-        ],
+        rof: .25,
         turnRate: 5,
-        drawShape: function() {
-            this.context.beginPath();
-            this.context.moveTo(-3, -5);
-            this.context.lineTo(12, 0);
-            this.context.lineTo(-3, 5);
-            this.context.lineTo(-1, 0);
-            this.context.closePath();
-            this.context.stroke();
-        },
-        shipShape: new Path2D('M-3 -5 L12 0 L-3 5 L-1 0 Z')
+        model: `
+            M  -3  -5
+            L  12   0
+            L  -3   5
+            L  -1   0
+            Z`
     }
 }
