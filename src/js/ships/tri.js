f8ab8e13bd1ec500901b5645df52ba3551f256c4
burntcustard <burntcustard@gmail.com>
2019-08-18 00:26:40 +0100

    Started implementing mass & thrust

--- src/js/ships/tri.js
+++ src/js/ships/tri.js
@@ -2,12 +2,14 @@
 export default () => {
     return {
         rof: .25,
-        turnRate: 5,
-        radius: 9,
+        turnRate: 6,
+        radius: 7,
+        mass: 4,
+        thrust: 5,
         lines: {
             body: [
-                [ -6, -5,  8,  0 ],
-                [  8,  0, -6,  5 ],
+                [ -6, -5,  7,  0 ],
+                [  7,  0, -6,  5 ],
                 [ -6,  5, -4,  0 ],
                 [ -4,  0, -6, -5 ]
             ]
