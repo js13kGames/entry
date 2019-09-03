a2842d21d3dfb373e174c236d2b776eb3afa3d42
burntcustard <burntcustard@gmail.com>
2019-09-03 08:14:07 +0100

    Trying to figure out what to put in a main.js for main menu screen

---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+/**
+ * Just figuring out WTF we want to do here ATM
+ */
+
+// Setup canvas stuff (probably w/Kontra)
+
+// have some sort of render loop which can rotate the logo or something cool
+
+// canvas.draw menu stuff:
+// big logo/splash message
+
+// menu buttons <ul> (but with canvas so not an <ul> I think)
+
+// when keyPressed 'down' or 'up' highlight menu item with diff color or somethin
+
+// when enter pressed, go to options scene, or ship-picking scene
+
+// have some way of getting back to the main menu scene!
