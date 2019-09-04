3393165d05e895acb48b55d2c859a757b976b462
Yinzhe_Qi <qiyzh@neusoft.com>
2019-09-04 18:06:57 +0800

    add start and over splash screen logic

--- src/index.js
+++ src/index.js
@@ -5,8 +5,33 @@ const canvas = document.getElementById('game')
 canvas.width = canvas.parentElement.clientWidth
 canvas.height = canvas.parentElement.clientHeight
 
-const game = new Game(canvas)
-game.onGameOver = () => {
-  alert('game over')
+// show splash
+const newGameSplash = document.getElementById('new-game')
+const gameOverSplash = document.getElementById('game-over')
+newGameSplash.style.opacity = 1
+
+// press Z to start
+document.addEventListener('keypress', checkStartKey)
+
+let game = null
+
+function checkStartKey(ev) {
+  if (ev.key === 'z') {
+    game = new Game(canvas)
+    game.onGameOver = () => {
+      gameOverSplash.style.opacity = 1
+      game = null
+      document.addEventListener('keypress', checkStartKey)
+    }
+
+    const splash = newGameSplash.style.opacity === '1' ? newGameSplash : gameOverSplash
+    const splashInterval = setInterval(() => {
+      splash.style.opacity -= 100 / 2000
+      if (splash.style.opacity <= 0) {
+        game.start()
+        clearInterval(splashInterval)
+        document.removeEventListener('keypress', checkStartKey)
+      }
+    }, 100)
+  }
 }
-game.start()
