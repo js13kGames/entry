1e3827d490be9d2723a9ad6ca9ce679dfdc0b9ed
Jerome Lecomte <jerle76@gmail.com>
2019-09-03 21:35:48 -0400

    WIP terrible code to try to start the game and reveal outlaws

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,16 @@
-console.log('hello WebXR ', AFRAME);
+AFRAME.registerSystem('game', {
+  init: function() {
+    this.titleEl = this.el.querySelector('#title');
+    this.outlawEls = this.el.querySelectorAll('.outlaw');
+
+    this.startGame = AFRAME.utils.bind(this.startGame, this);
+    this.el.querySelector('#start').addEventListener('mouseenter', this.startGame);
+  },
+  startGame: function(evt) {
+    console.log('startGame');
+    this.titleEl.setAttribute('visible', false);
+    this.outlawEls.forEach(function(outlawEl) {
+      outlawEl.setAttribute('visible', true);
+    })
+  }
+});
