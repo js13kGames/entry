8cfd5be105948ef432cfc491ed1050bd83397550
Amy Frieson <ava447@gmail.com>
2019-09-03 20:58:33 -0400

    ♻️  Refactor setTimeouts out of audio 🎶

--- src/audio-controller.pkg.js
+++ src/audio-controller.pkg.js
@@ -3,36 +3,70 @@ function playMusic () {
   const masterGain = audioCtx.createGain();
   masterGain.gain.value = 0.5;
 
-  function playNote(freq = 200, sweepLength = 1, wave = 'sine') {
+  let lastScheduledNote = {
+    drum: 1,
+    synth: 1
+  }
+
+  function makeNote(startTime, freq, sweepLength, wave) {
     var oscillator = audioCtx.createOscillator();
     oscillator.type = wave;
 
     let sweepEnv = audioCtx.createGain();
-    sweepEnv.gain.cancelScheduledValues(audioCtx.currentTime);
-    sweepEnv.gain.setValueAtTime(0, audioCtx.currentTime);
+    sweepEnv.gain.cancelScheduledValues(startTime);
+    sweepEnv.gain.setValueAtTime(0, startTime);
     // set our attack
-    sweepEnv.gain.linearRampToValueAtTime(0.5, audioCtx.currentTime + 0.01);
+    sweepEnv.gain.linearRampToValueAtTime(0.5, startTime + 0.01);
     // set our release
-    sweepEnv.gain.linearRampToValueAtTime(0, audioCtx.currentTime + sweepLength - 0.5);
+    sweepEnv.gain.linearRampToValueAtTime(0, startTime + sweepLength - 0.5);
 
-    oscillator.frequency.setValueAtTime(freq, audioCtx.currentTime); // value in hertz
+    oscillator.frequency.setValueAtTime(freq, startTime); // value in hertz
     oscillator.connect(sweepEnv).connect(masterGain).connect(audioCtx.destination);
-    oscillator.start();
-    oscillator.stop(audioCtx.currentTime + sweepLength);
+    oscillator.start(startTime);
+    oscillator.stop(startTime + sweepLength);
   }
 
-  (function playNotes() {
-    const intervals = [250, 500, 750, 1000, 1250, 1500];
-    const frequencies = [200, 236, 284, 300];
-    let time = Math.floor(Math.random() * intervals.length);
-    const freq = Math.floor(Math.random() * frequencies.length);
-    const sweepLength = Math.random() * 10;
-    playNote(frequencies[freq], sweepLength, 'triangle');
-    setTimeout(playNotes, intervals[time]);
-  })();
+  function makeDrumNote(startTime) {
+    makeNote(startTime, 200, 1, 'sine');
+  }
 
-  (function playDrum() {
-    setInterval(playNote, 500);
-  })();
+  function makeSynthNote(startTime) {
+    const frequencies = [200, 236, 284, 300];
+    makeNote(
+      startTime,
+      frequencies[floor(random() * frequencies.length)],
+      random() * 10 + 1,
+      'triangle'
+    );
+  }
+
+  function getNextStartTime(interval, key) {
+    return lastScheduledNote[key] + interval;
+  }
+
+  function scheduleNextNote(interval, makeSound, key) {
+    const currentTime = audioCtx.currentTime;
+    while ((currentTime + 2) > lastScheduledNote[key]) {
+      const nextTime = getNextStartTime(interval, key);
+      makeSound(nextTime);
+      lastScheduledNote[key] = nextTime;
+    }
+  }
+
+  function scheduleInstruments() {
+    // drum
+    scheduleNextNote(.5, makeDrumNote, 'drum');
+
+    // generative
+    const intervals = [.25, .5, .75, 1, 1.25, 1.5];
+    scheduleNextNote(
+      intervals[floor(random() * intervals.length)],
+      makeSynthNote,
+      'synth'
+    );
+  }
+
+  setInterval(scheduleInstruments, 100);
+
   return true;
 }
