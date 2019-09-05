4807cd908f8143ad7e745584019055446b181118
Jerome Lecomte <jerle76@gmail.com>
2019-09-04 21:43:14 -0400

    make targets shootable and revivable with a custom component

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,66 @@
-AFRAME.registerSystem('game', {
-  init: function() {
-    this.titleEl = this.el.querySelector('#title');
-    this.outlawEls = this.el.querySelectorAll('.outlaw');
 
-    this.startGame = AFRAME.utils.bind(this.startGame, this);
-    this.el.querySelector('#start').addEventListener('mouseenter', this.startGame);
+AFRAME.registerComponent('target', {
+  schema: {
+    revive: { type: 'selectorAll' },
   },
-  startGame: function(evt) {
-    console.log('startGame');
-    this.titleEl.setAttribute('visible', false);
-    this.outlawEls.forEach(function(outlawEl) {
-      outlawEl.setAttribute('visible', true);
-    })
-  }
+  init: function() {
+    // state variables
+    this.isUp = true;
+    this.isAnimating = false;
+
+    // add target entity within
+    this.targetEl = document.createElement('a-ring');
+    this.targetEl.setAttribute('color', 'red');
+    this.targetEl.setAttribute('radius-inner', 0.1);
+    this.targetEl.setAttribute('radius-outer', 0.6);
+    this.targetEl.object3D.position.set(0, 0.6, 0);
+    this.el.appendChild(this.targetEl);
+
+    this.onShot = AFRAME.utils.bind(this.onShot, this);
+    this.onDeadOrAlive = AFRAME.utils.bind(this.onDeadOrAlive, this);
+    this.onRevive = AFRAME.utils.bind(this.onRevive, this);
+
+    this.el.addEventListener('mouseenter', this.onShot);
+  },
+  onShot: function() {
+    if (this.isUp && !this.isAnimating) {
+      this.isAnimating = true;
+
+      this.el.setAttribute('animation', {
+        property: 'object3D.rotation.x',
+        from: 0,
+        to: -90,
+        dur: 1000,
+        easing: 'easeOutElastic',
+        elasticity: 800,
+      });
+      this.el.addEventListener('animationcomplete', this.onDeadOrAlive);
+
+      this.data.revive.forEach(function(targetEl) {
+        targetEl.components.target.onRevive();
+      });
+    }
+  },
+  onDeadOrAlive: function() {
+    this.isAnimating = false;
+    this.isUp = !this.isUp;
+    this.el.removeAttribute('animation');
+    this.el.removeEventListener('animationcomplete', this.onDeadOrAlive);
+  },
+  onRevive: function() {
+    if (!this.isUp && !this.isAnimating) {
+      this.isAnimating = true;
+
+      this.el.setAttribute('animation', {
+        property: 'object3D.rotation.x',
+        from: -90,
+        to: 0,
+        dur: 1000,
+        easing: 'easeOutElastic',
+        elasticity: 800,
+      });
+
+      this.el.addEventListener('animationcomplete', this.onDeadOrAlive);
+    }
+  },
 });
