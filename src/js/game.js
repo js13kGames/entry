39f0754ca5ad79d33b3506858a200458979aed51
burntcustard <burntcustard@gmail.com>
2019-09-08 19:36:56 +0100

    Quite messy but working entire game/menus state

--- src/js/game.js
+++ src/js/game.js
@@ -1,200 +1,221 @@
-import { init, GameLoop, initKeys, keyPressed } from 'kontra';
-import { Collisions } from 'collisions';
-import { doCollisions } from './doCollisions';
-import { initGamepads, pollGamepads, buttonPressed, axisValue } from './gamepad';
-import { Ship } from './ship.js';
-import { AmmoPickup } from './pickups/ammo.js';
-import { ShieldPickup } from './pickups/shield.js';
-import { StarPickup } from './pickups/star.js';
-import { Player } from './player.js';
-import { createMeteor } from './meteor';
-import * as gameOver from './gameOver';
-
-
-function endGame() {
-    // Pause the game and put win screen up?
-    game.over = true;
-    game.places = [];
-    // Give each player a position (1st, 2nd, etc)
-    game.players.forEach(player => {
-        game.places.push(player);
-    });
-    game.places.sort((p1, p2) => {
-        if (p1.score > p2.score) {
-            return -1;
-        }
-        if (p1.score < p2.score) {
-            return 1;
-        }
-        // Scores are the same so compare deaths
-        if (p1.deaths < p2.deaths) {
-            return -1;
-        }
-        if (p1.deaths > p2.deaths) {
-            return 1;
-        }
-        // Deaths are the same as well, so return random
-        return Math.round(Math.random() < .5 ? -1 : 1);
-    });
-    game.places.forEach((player, i) => {
-        player.place = i;
-    });
-}
-
-const gameLoop = GameLoop({  // create the main game loop
-    update() { // update the game state
-        pollGamepads();
-
-        if (game.over) {
-            game.over++;
-        }
-
-        game.players.forEach(player => {
-            player.update();
-        });
-
-        game.sprites.map(sprite => {
-            sprite.update();
-
-            if (sprite.type !== 'bullet') {
-                if (sprite.x > game.width) {
-                    sprite.x = 0;
-                } else if (sprite.x < 0) {
-                    sprite.x = game.width;
-                }
-                if (sprite.y > game.height) {
-                    sprite.y = 0;
-                } else if (sprite.y < 0) {
-                    sprite.y = game.height;
-                }
-            }
-        });
-
-        doCollisions(game);
-
-        // Remove dead & exploded sprite's hitboxes from the collision system
-        game.sprites.forEach(sprite => {
-            if (!sprite.isAlive() || sprite.exploded) {
-                sprite.hitbox && sprite.hitbox.remove();
-            }
-        });
-
-        // Remove dead sprites from the sprites list
-        game.sprites = game.sprites.filter(sprite => sprite.isAlive());
-        game.meteors = game.meteors.filter(sprite => sprite.isAlive());
-        game.pickups = game.pickups.filter(sprite => sprite.isAlive());
-
-        // Remove exploded ships from the both lists & the player
-        game.sprites = game.sprites.filter(sprite => !sprite.exploded);
-
-        if (!game.over) {
-            game.players.forEach(player => {
-                if (player.ship.exploded && player.ship.ttl) {
-                    player.ship = {};
-                    setTimeout(() => {
-                        player.respawn();
-                    }, 3000);
-                }
-
-                // Check if someone has won!
-                // TODO: See if it's likely for 2 ships to get to 10 in same update
-                if (player.score === 1) {
-                    endGame();
-                }
-            });
-        }
-
-        if (game.meteors.length < 5 && Math.random() < .005) {
-            createMeteor({
-                x: Math.random() * game.width,
-                y: Math.random() * game.height,
-                radius: 20,
-                game: game
-            });
-        }
-
-        if (game.pickups.length < 1) {
-            let rand = Math.random();
-            let pickup = null;
-            if (.000 < rand && rand < .001) {
-                pickup = new AmmoPickup({
-                    x: Math.random() * game.width,
-                    y: Math.random() * game.height,
-                    game: game
-                });
-            }
-            if (.001 < rand && rand < .002) {
-                pickup = new ShieldPickup({
-                    x: Math.random() * game.width,
-                    y: Math.random() * game.height,
-                    game: game
-                });
-            }
-            if (.002 < rand && rand < .003) {
-                pickup = new StarPickup({
-                    x: Math.random() * game.width,
-                    y: Math.random() * game.height,
-                    game: game
-                });
-            }
-            if (pickup) {
-                game.sprites.push(pickup);
-                game.pickups.push(pickup);
-            }
-        }
-
-    },
-
-    render() {
-        // Render all the sprites
-        game.sprites.map(sprite => sprite.render(game.scale));
-
-        // Render the player scores
-        game.players.map((player, i) => player.renderScore(i));
-
-        if (game.over) {
-            gameOver.render(game);
-        }
-
-        // Render debug collision stuff
-        game.context.save();
-        game.context.scale(game.scale, game.scale);
-        game.context.strokeStyle = '#0F0';
-        game.context.beginPath();
-        game.cSystem.draw(game.context);
-        game.context.stroke();
-        game.context.restore();
-    }
-});
-
-var game;
-
-export default function startGame(newGame, canvas, context) {
-    game = newGame;
-
-    game.meteors = [];
-    game.pickups = [];
-    game.sprites = [];
-
-    // Create new collision system & collision result object
-    game.cSystem = new Collisions();
-    game.cResult = game.cSystem.createResult();
-
-    // Big asteroid in the middle (dioretsa)
-    createMeteor({
-        x: game.width / 2,
-        y: game.height / 2,
-        radius: Math.min(game.width / 5, game.height / 5),
-        mass: 100000,
-        dx: 0,
-        dy: 0,
-        dr: .1,
-        game: game
-    });
-
-    game.players.forEach(player => {
-        player.spawn();
-    });
-
-    gameLoop.start();
-}
+// import { init, GameLoop, initKeys, keyPressed } from 'kontra';
+// import { Collisions } from 'collisions';
+// import { doCollisions } from './doCollisions';
+// import { initGamepads, pollGamepads, buttonPressed, axisValue } from './gamepad';
+// import { Ship } from './ship.js';
+// import { AmmoPickup } from './pickups/ammo.js';
+// import { ShieldPickup } from './pickups/shield.js';
+// import { StarPickup } from './pickups/star.js';
+// import { Player } from './player.js';
+// import { createMeteor } from './meteor';
+// import * as gameOver from './gameOver';
+//
+//
+// function endGame() {
+//     // Pause the game and put win screen up?
+//     game.over = true;
+//     game.places = [];
+//     // Give each player a position (1st, 2nd, etc)
+//     game.players.forEach(player => {
+//         game.places.push(player);
+//     });
+//     game.places.sort((p1, p2) => {
+//         if (p1.score > p2.score) {
+//             return -1;
+//         }
+//         if (p1.score < p2.score) {
+//             return 1;
+//         }
+//         // PLayer scores are the same so compare deaths
+//         if (p1.deaths < p2.deaths) {
+//             return -1;
+//         }
+//         if (p1.deaths > p2.deaths) {
+//             return 1;
+//         }
+//         // Player deaths are the same as well! So choose a player randomly
+//         return Math.round(Math.random() < .5 ? -1 : 1);
+//     });
+//     game.places.forEach((player, i) => {
+//         player.place = i;
+//     });
+// }
+//
+// const gameLoop = GameLoop({  // create the main game loop
+//     update() { // update the game state
+//         pollGamepads();
+//
+//         if (game.over) {
+//             game.over++;
+//
+//             // Check for "readying to go to menu"
+//
+//             // Players are already .ready from game start, so check for unready
+//             game.players.forEach(player => {
+//                 if (player.keys.accept()) {
+//                     player.debounce.accept--;
+//                     if (player.debounce.accept <= 0) {
+//                         player.ready = !player.ready;
+//                         player.debounce.accept = 15;
+//                     }
+//                 } else {
+//                     player.debounce.accept = 0;
+//                 }
+//             });
+//
+//             if (game.players.every(p => !p.ready)) {
+//                 console.log("returning to main menu");
+//                 gameLoop.stop();
+//                 mainMenuLoop.start(); // How do I get this var???
+//             }
+//         }
+//
+//         game.players.forEach(player => {
+//             player.update();
+//         });
+//
+//         game.sprites.map(sprite => {
+//             sprite.update();
+//
+//             if (sprite.type !== 'bullet') {
+//                 if (sprite.x > game.width) {
+//                     sprite.x = 0;
+//                 } else if (sprite.x < 0) {
+//                     sprite.x = game.width;
+//                 }
+//                 if (sprite.y > game.height) {
+//                     sprite.y = 0;
+//                 } else if (sprite.y < 0) {
+//                     sprite.y = game.height;
+//                 }
+//             }
+//         });
+//
+//         doCollisions(game);
+//
+//         // Remove dead & exploded sprite's hitboxes from the collision system
+//         game.sprites.forEach(sprite => {
+//             if (!sprite.isAlive() || sprite.exploded) {
+//                 sprite.hitbox && sprite.hitbox.remove();
+//             }
+//         });
+//
+//         // Remove dead sprites from the sprites list
+//         game.sprites = game.sprites.filter(sprite => sprite.isAlive());
+//         game.meteors = game.meteors.filter(sprite => sprite.isAlive());
+//         game.pickups = game.pickups.filter(sprite => sprite.isAlive());
+//
+//         // Remove exploded ships from the both lists & the player
+//         game.sprites = game.sprites.filter(sprite => !sprite.exploded);
+//
+//         if (!game.over) {
+//             game.players.forEach(player => {
+//                 if (player.ship.exploded && player.ship.ttl) {
+//                     player.ship = {};
+//                     setTimeout(() => {
+//                         player.respawn();
+//                     }, 3000);
+//                 }
+//
+//                 // Check if someone has won!
+//                 // TODO: See if it's likely for 2 ships to get to 10 in same update
+//                 if (player.score === 1) {
+//                     endGame();
+//                 }
+//             });
+//         }
+//
+//         if (game.meteors.length < 5 && Math.random() < .005) {
+//             createMeteor({
+//                 x: Math.random() * game.width,
+//                 y: Math.random() * game.height,
+//                 radius: 20,
+//                 game: game
+//             });
+//         }
+//
+//         if (game.pickups.length < 1) {
+//             let rand = Math.random();
+//             let pickup = null;
+//             if (.000 < rand && rand < .001) {
+//                 pickup = new AmmoPickup({
+//                     x: Math.random() * game.width,
+//                     y: Math.random() * game.height,
+//                     game: game
+//                 });
+//             }
+//             if (.001 < rand && rand < .002) {
+//                 pickup = new ShieldPickup({
+//                     x: Math.random() * game.width,
+//                     y: Math.random() * game.height,
+//                     game: game
+//                 });
+//             }
+//             if (.002 < rand && rand < .003) {
+//                 pickup = new StarPickup({
+//                     x: Math.random() * game.width,
+//                     y: Math.random() * game.height,
+//                     game: game
+//                 });
+//             }
+//             if (pickup) {
+//                 game.sprites.push(pickup);
+//                 game.pickups.push(pickup);
+//             }
+//         }
+//
+//     },
+//
+//     render() {
+//         // Render all the sprites
+//         game.sprites.map(sprite => sprite.render(game.scale));
+//
+//         // Render the player scores
+//         game.players.map((player, i) => player.renderScore(i));
+//
+//         if (game.over) {
+//             gameOver.render(game);
+//         }
+//
+//         // Render debug collision stuff
+//         game.context.save();
+//         game.context.scale(game.scale, game.scale);
+//         game.context.strokeStyle = '#0F0';
+//         game.context.beginPath();
+//         game.cSystem.draw(game.context);
+//         game.context.stroke();
+//         game.context.restore();
+//     }
+// });
+//
+// var game;
+//
+// export default function startGame(newGame, canvas, context) {
+//     game = newGame;
+//
+//     game.meteors = [];
+//     game.pickups = [];
+//     game.sprites = [];
+//
+//     // Create new collision system & collision result object
+//     game.cSystem = new Collisions();
+//     game.cResult = game.cSystem.createResult();
+//
+//     // Big asteroid in the middle (dioretsa)
+//     createMeteor({
+//         x: game.width / 2,
+//         y: game.height / 2,
+//         radius: Math.min(game.width / 5, game.height / 5),
+//         mass: 100000,
+//         dx: 0,
+//         dy: 0,
+//         dr: .1,
+//         game: game
+//     });
+//
+//     game.players.forEach(player => {
+//         player.spawn();
+//     });
+//
+//     gameLoop.start();
+// }
--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,12 @@
-/**
- * Just figuring out WTF we want to do here ATM
- */
+import { init, initKeys } from 'kontra';
+import { initGamepads } from './gamepad';
 
-import { init, GameLoop, initKeys, keyPressed } from 'kontra';
-import { initGamepads, pollGamepads, buttonPressed, axisValue } from './gamepad';
-import { colors } from './colors';
-import ships from './ships/import';
-import { Player } from './player';
-import { Menu } from './menu';
-import { renderText } from './text';
-import * as util from './utility';
-import startGame from './game';
+// Import all the scenes because we're somehow going to handle changing
+// between them here... to stop circular dependencies getting in the way
+
+import { startMainMenu } from './scenes/main';
+import { startShipSelect } from './scenes/select';
+import { startGame } from './scenes/game';
 
 // Setup canvas stuff (probably w/Kontra)
 
@@ -23,6 +19,12 @@ initKeys();
 // Init gamepad event listeners n stuff
 initGamepads();
 
+const scenes = {
+    startMainMenu: startMainMenu,
+    startShipSelect: startShipSelect,
+    startGame: startGame
+};
+
 // window.addEventListener('gamepadconnected', function(e) {
 //     console.log(`!Gamepad connected at index ${pad.index}: ${pad.id}. ${pad.buttons.length} buttons, ${pad.axes.length} axes.`);
 // })
@@ -30,6 +32,7 @@ initGamepads();
 canvas.style = 'width:100%;background:#000';
 
 const game = {
+    canvas: canvas,
     context: context,
     // meteors: [],
     // pickups: [],
@@ -69,258 +72,4 @@ window.onresize = () => {
     setSizing(game.supersample);
 }
 
-var mainMenu = new Menu({
-    context: context,
-    x: 30,
-    y: 30,
-    color: '#fff',
-    items: [
-        { text: 'play' },
-        { text: 'settings' },
-        { text: 'credits' }
-    ]
-});
-
-game.players.push(new Player({
-    color: colors.yellow,
-    shipType: 'tri',
-    controls: 'arrows',
-    context: context,
-    game: game
-}));
-
-game.players.push(new Player({
-    color: colors.red,
-    shipType: 'coback',
-    controls: 'wasd',
-    context: context,
-    game: game
-}));
-
-game.players.push(new Player({
-    color: colors.blue,
-    shipType: 'coback',
-    controls: 'wasd',
-    context: context,
-    game: game
-}));
-
-game.players.push(new Player({
-    color: colors.orange,
-    shipType: 'coback',
-    controls: 'wasd',
-    context: context,
-    game: game
-}));
-
-//
-// game.players.push(new Player({
-//     color: colors.blue,
-//     shipType: 'striker',
-//     controls: 'gamepad',
-//     gamepadIndex: 0,
-//     context: context,
-//     game: game
-// }));
-
-let mainMenuLoop = GameLoop({  // create the main game loop
-    update() { // update the game state
-        pollGamepads();
-
-        game.players.forEach(player => {
-
-            if (player.keys.up()) {
-                if (player.debounce.up > 0) {
-                    player.debounce.up--;
-                    return;
-                }
-                mainMenu.prev();
-                player.debounce.up = 15;
-            } else {
-                player.debounce.up = 0;
-            }
-
-            if (player.keys.down()) {
-                player.debounce.down--;
-                if (player.debounce.down <= 0) {
-                    mainMenu.next();
-                    player.debounce.down = 15;
-                }
-            } else {
-                player.debounce.down = 0;
-            }
-
-            if (player.keys.accept()) {
-                mainMenuLoop.stop()
-                if (mainMenu.items[mainMenu.focus].text === 'play') {
-                    game.players.forEach(player => {
-                        player.pseudoSpawn();
-                    });
-                    shipMenuLoop.start();
-                    //startGame(game, canvas, context);
-                }
-            }
-        });
-    },
-
-    render() {
-        mainMenu.render(game.scale);
-    }
-});
-
-mainMenuLoop.start();
-
-playerMenus = [];
-
-let shipMenuLoop = GameLoop({  // create the main game loop
-    update() { // update the game state
-        pollGamepads();
-
-        game.players.forEach((player, i) => {
-
-            if (player.keys.accept()) {
-                player.debounce.accept--;
-                if (player.debounce.accept <= 0) {
-                    player.ready = !player.ready;
-                    player.debounce.accept = 15;
-                }
-            } else {
-                player.debounce.accept = 0;
-            }
-
-            if (player.ready) {
-                return;
-            }
-
-            player.ship.rotation += player.ship.dr;
-
-            if (player.keys.up()) {
-                if (player.debounce.up > 0) {
-                    player.debounce.up--;
-                    return;
-                }
-
-                player.color = util.objValPrev(colors, player.color);
-
-                while (util.otherPlayerHasSameColor(game.players, player)) {
-                    player.color = util.objValPrev(colors, player.color);
-                }
-
-                game.players[i].ship.color = player.color;
-                player.debounce.up = 15;
-            } else {
-                player.debounce.up = 0;
-            }
-
-            if (player.keys.down()) {
-                if (player.debounce.down > 0) {
-                    player.debounce.down--;
-                    return;
-                }
-
-                player.color = util.objValNext(colors, player.color);
-
-                while (util.otherPlayerHasSameColor(game.players, player)) {
-                    player.color = util.objValNext(colors, player.color);
-                }
-
-                game.players[i].ship.color = player.color;
-                player.debounce.down = 15;
-            } else {
-                player.debounce.down = 0;
-            }
-
-            if (player.keys.left()) {
-                if (player.debounce.left > 0) {
-                    player.debounce.left--;
-                    return;
-                }
-                player.shipType = util.objKeyPrev(ships, player.shipType);
-                player.pseudoSpawn();
-                player.debounce.left = 15;
-            } else {
-                player.debounce.left = 0;
-            }
-
-            if (player.keys.right()) {
-                if (player.debounce.right > 0) {
-                    player.debounce.right--;
-                    return;
-                }
-                player.shipType = util.objKeyNext(ships, player.shipType);
-                player.pseudoSpawn();
-                player.debounce.right = 15;
-            } else {
-                player.debounce.right = 0;
-            }
-
-        });
-
-        if (game.players.every(player => player.ready)) {
-            shipMenuLoop.stop();
-            startGame(game, canvas, context);
-        }
-    },
-
-    render() {
-        game.players.forEach((player, i) => {
-            var x = y = 0;
-
-            if (i === 1 || i == 3) {
-                x = 4 + game.width / 2;
-            } else {
-                x = 8;
-            }
-            if (i === 2 || i === 3) {
-                y = 4 + game.height / 2;
-            } else {
-                y = 8;
-            }
-
-            context.save();
-            context.scale(game.scale, game.scale);
-            context.clearRect(
-                x,
-                y,
-                game.width / 2 - 12,
-                game.height / 2 - 12,
-            );
-            context.strokeStyle = player.color;
-            context.strokeRect(
-                x,
-                y,
-                game.width / 2 - 12,
-                game.height / 2 - 12,
-            )
-            context.restore();
-
-            renderText({
-                text: 'player ' + (i + 1),
-                color: player.color,
-                size: .8,
-                x: x + 12,
-                y: y + 12,
-                scale: game.scale,
-                context: context
-            });
-
-            renderText({
-                alignRight: true,
-                alignBottom: true,
-                text: player.ready ? 'ready!' : 'selecting',
-                color: player.color,
-                size: .8,
-                x: x + game.width / 2 - 22,
-                y: y + game.height / 2 - 20,
-                scale: game.scale,
-                context: context
-            });
-
-            player.ship.pseudoRender(
-                game.scale,
-                x + game.width / 2 - 80,
-                y + (game.height / 2 - 12) / 2
-            );
-        });
-    }
-});
+startMainMenu(game, scenes);
