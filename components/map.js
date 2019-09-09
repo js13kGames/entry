453a6b253bdb7097c002aac9c0d9da84ec225660
Reece Bennett <reece.bennett1@gmail.com>
2019-09-09 21:11:50 +0100

    Keep map centered on location, only allow valid targets

--- components/map.js
+++ components/map.js
@@ -4,27 +4,30 @@ AFRAME.registerComponent("map", {
 
     const el = this.el;
     const sceneEl = el.sceneEl;
+    const mapObjects = this.el.querySelector("#mo");
 
     const shipEl = sceneEl.querySelector("#ship");
     let ship;
     shipEl.addEventListener("loaded", e => ship = shipEl.components.ship);
 
-    sceneEl.addEventListener("worldReady", e => {
-      const world = e.detail;
-      console.log(world);
+    let offset = {x: 0, y: 0};
+    let world;
+
+    function draw() {
+      mapObjects.innerHTML = "";
 
       for (let node of world.nodes) {
         const nodeEl = document.createElement("a-circle");
         nodeEl.setAttribute("color", node.color);
         nodeEl.setAttribute("geometry", {radius: node.size});
-        nodeEl.setAttribute("position", {x: node.x, y: node.y, z: 0.002});
+        nodeEl.setAttribute("position", {x: node.x + offset.x, y: node.y + offset.y, z: 0.002});
         nodeEl.className = "clickable";
         nodeEl.addEventListener("click", e => {
-          if (ship.location != node) {
+          if (ship.location != node && world.links.find(l => l.includes(node.name) && l.includes(ship.location.name))) {
             nodeEl.emit("changeTarget", node);
           }
         });
-        el.appendChild(nodeEl);
+        mapObjects.appendChild(nodeEl);
       }
 
       for (let link of world.links) {
@@ -40,55 +43,64 @@ AFRAME.registerComponent("map", {
           width: 0.01,
           height: Math.sqrt(aj*aj + op*op)
         });
-        linkEl.object3D.position.set(start.x + aj/2, start.y + op/2, 0.001);
+        linkEl.object3D.position.set(start.x + aj/2 + offset.x, start.y + op/2 + offset.y, 0.001);
         linkEl.object3D.rotation.z = Math.atan2(op, aj) - Math.PI / 2;
         linkEl.setAttribute("material", {color: "#fff"});
-        el.appendChild(linkEl);
+        mapObjects.appendChild(linkEl);
+      }
+    }
+
+    sceneEl.addEventListener("worldReady", e => {
+      world = e.detail;
+      console.log(world);
+      draw();
+    });
+
+    const locationEl = this.el.querySelector("#location");
+    this.el.sceneEl.addEventListener("changeLocation", e => {
+      const n = e.detail;
+
+      if (n) {
+        offset = {x: -n.x, y: -n.y};
+        draw();
+
+        locationEl.object3D.position.set(n.x, n.y, .003);
+        locationEl.object3D.visible = true;
+
+        const off = Math.max(n.size + 0.01, 0.03);
+        const a = off - 0.01;
+
+        locationEl.children[0].object3D.position.set(a + offset.x, off + offset.y, 0);
+        locationEl.children[1].object3D.position.set(off + offset.x, a + offset.y, 0);
+
+        locationEl.children[2].object3D.position.set(a + offset.x, -off + offset.y, 0);
+        locationEl.children[3].object3D.position.set(off + offset.x, -a + offset.y, 0);
+
+        locationEl.children[4].object3D.position.set(-a + offset.x, -off + offset.y, 0);
+        locationEl.children[5].object3D.position.set(-off + offset.x, -a + offset.y, 0);
+
+        locationEl.children[6].object3D.position.set(-a + offset.x, off + offset.y, 0);
+        locationEl.children[7].object3D.position.set(-off + offset.x, a + offset.y, 0);
+      } else {
+        locationEl.object3D.visible = false;
       }
     });
 
     const targetEl = this.el.querySelector("#target");
-    this.el.sceneEl.addEventListener("changeLocation", e => {
+    this.el.sceneEl.addEventListener("changeTarget", e => {
       const n = e.detail;
 
       if (n) {
         targetEl.object3D.position.set(n.x, n.y, .003);
         targetEl.object3D.visible = true;
 
-        const off = Math.max(n.size + 0.01, 0.03);
-        const a = off - 0.01;
-
-        targetEl.children[0].object3D.position.set(a, off, 0);
-        targetEl.children[1].object3D.position.set(off, a, 0);
-
-        targetEl.children[2].object3D.position.set(a, -off, 0);
-        targetEl.children[3].object3D.position.set(off, -a, 0);
-
-        targetEl.children[4].object3D.position.set(-a, -off, 0);
-        targetEl.children[5].object3D.position.set(-off, -a, 0);
-
-        targetEl.children[6].object3D.position.set(-a, off, 0);
-        targetEl.children[7].object3D.position.set(-off, a, 0);
+        const off = Math.max(n.size + 0.02, 0.03);
+        targetEl.children[0].object3D.position.set(0 + offset.x, off + offset.y, 0);
+        targetEl.children[1].object3D.position.set(off + offset.x, 0 + offset.y, 0);
+        targetEl.children[2].object3D.position.set(0 + offset.x, -off + offset.y, 0);
+        targetEl.children[3].object3D.position.set(-off + offset.x, 0 + offset.y, 0);
       } else {
         targetEl.object3D.visible = false;
-      }
-    });
-
-    const locationEl = this.el.querySelector("#location");
-    this.el.sceneEl.addEventListener("changeTarget", e => {
-      const n = e.detail;
-
-      if (n) {
-        locationEl.object3D.position.set(n.x, n.y, .003);
-        locationEl.object3D.visible = true;
-
-        const off = Math.max(n.size + 0.02, 0.03);
-        locationEl.children[0].object3D.position.set(0, off, 0);
-        locationEl.children[1].object3D.position.set(off, 0, 0);
-        locationEl.children[2].object3D.position.set(0, -off, 0);
-        locationEl.children[3].object3D.position.set(-off, 0, 0);
-      } else {
-        locationEl.object3D.visible = false;
       }
     });
   }
