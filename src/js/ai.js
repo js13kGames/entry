42d219c4216c408e45a9212732227b4adc6cb730
burntcustard <burntcustard@gmail.com>
2019-09-11 00:57:07 +0100

    AI improvements

--- src/js/ai.js
+++ src/js/ai.js
@@ -28,38 +28,88 @@ function getAngle(ai, target, inverse) {
     return angle;
 }
 
+function maybeRewind(ai) {
+    var maybeRewind = 0; // A % chance the AI will use rewind this update
+
+    // Don't rewind if already rewinding
+    if (ai.ship.rewiding) {
+        return false;
+    }
+
+    // Don't rewind if it's still on cooldown
+    if (ai.ship.rewindDt <= ai.ship.ror) {
+        return false
+    }
+
+    // Rewind to get ammo back (none to at least full if possible)
+    if (ai.ship.ammoCurrent < 1 && ai.ship.history[0].ammo >= ai.ship.ammo) {
+        maybeRewind += 8;
+    }
+
+    // Rewing is more likely the less time is left on rainbow-star powerup
+    //  - that way the AI is more likely to "extend" it's duration with rewind
+    // Only does it is at least half way through rainbow-ing
+    if (ai.ship.rainbow && ai.ship.rainbow < 4) {
+        maybeRewind += 6;
+    }
+
+    if (Math.random() * 100 < maybeRewind) {
+        ai.ship.rewind();
+        return true;
+    }
+}
+
 export function update(ai) {
     //var otherPlayers = ai.game.players.filter(p => p !== ai);
 
     // Currently the AI will only fight player 1 and look for pickup 1
     var player = game.players[0];
-    //var pickup = game.pickups[0];
-    var pickup = null;
+    var pickup = game.pickups[0];
 
     if (!ai.ship || !ai.ship.isAlive || ai.game.over) {
         return;
     }
 
+    if (maybeRewind(ai)) {
+        return;
+    }
+
     var avoidingMeteor = false;
 
-    game.meteors.forEach(meteor => {
+    game.meteors.forEach((meteor, i) => {
         if (getDist(ai, meteor) > meteor.radius + 40) {
             return;
         }
 
-        avoidingMeteor = true;
-
         var angleToMeteor = getAngle(ai, meteor)
 
-        if (angleToMeteor < -1) {
-            ai.ship.turnRight();
-            ai.ship.engineOff();
-        } else if (angleToMeteor > 1) {
-            ai.ship.turnLeft();
-            ai.ship.engineOff();
-        } else {
+        // Avoid the big meteor in the middle when rainbowy,
+        // but don't slow or turn much
+        if (ai.ship.rainbow > 1 && i === 0) {
+            avoidingMeteor = true;
+
             ai.ship.engineOn();
+            if (angleToMeteor < -2) {
+                ai.ship.turnRight();
+            } else if (angleToMeteor > 2) {
+                ai.ship.turnLeft();
+            }
+
+        // Avoid all meteots when not rainbowy (or not rainbowy for > 1s)
+        } else if (ai.ship.rainbow < 1) {
+            avoidingMeteor = true;
+
+            if (angleToMeteor < -1) {
+                ai.ship.turnRight();
+                ai.ship.engineOff();
+            } else if (angleToMeteor > 1) {
+                ai.ship.turnLeft();
+                ai.ship.engineOff();
+            } else {
+                ai.ship.engineOn();
+            }
         }
+        // Otherwise avoidingMeteor is still false
     });
 
     if (avoidingMeteor) {
@@ -67,11 +117,9 @@ export function update(ai) {
     }
 
     if (player.ship && player.ship.isAlive) {
-        if (player.ship && player.ship.isAlive) {
-            ai.distToPlayer = getDist(ai, player.ship);
-        } else {
-            ai.distToPlayer = null;
-        }
+        ai.distToPlayer = getDist(ai, player.ship);
+    } else {
+        ai.distToPlayer = null;
     }
 
     if (pickup) {
@@ -94,19 +142,30 @@ export function update(ai) {
         return;
     }
 
-    ai.angleToTarget = getAngle(ai, ai.target, true);
+    // Get the angle to the target (player or pickup), but if the target
+    // is rainbowy, get the angle away from. RUN AWAY!
+    ai.angleToTarget = getAngle(ai, ai.target, !ai.target.rainbow);
 
     if (ai.angleToTarget < -.1) {
         ai.ship.turnRight();
     } else if (ai.angleToTarget > .1) {
         ai.ship.turnLeft();
     } else {
-        if (ai.distToPlayer < 100) {
-            if (Math.random() < .1) { ai.ship.fire(); }
-            if (Math.random() < .01) { ai.ship.engineOff(); }
-            if (Math.random() < .01) { ai.ship.engineOn(); }
-        } else {
+        // If the target is rainbowy, just run away,
+        // or if we are, go TOWARDS target
+        if (ai.target.rainbow || ai.ship.rainbow) {
             ai.ship.engineOn();
+        // Otherwise...
+        } else {
+            // If target is nearby player, randomly-ish shoot and toggle engine
+            if (ai.target === player.ship && ai.distToPlayer < 100) {
+                if (Math.random() < .1) { ai.ship.fire(); }
+                if (Math.random() < .01) { ai.ship.engineOff(); }
+                if (Math.random() < .01) { ai.ship.engineOn(); }
+            // Else target is power up, just go towards it
+            } else {
+                ai.ship.engineOn();
+            }
         }
     }
 
