22c16a09ea34609e5b9c2dcdb8ea49aaaf9d29b5
Fawwaz <fawwazmuhammad@gmail.com>
2019-09-12 01:39:36 +0700

    initial setup

---
+++ rollup.config.js
@@ -0,0 +1,23 @@
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import typescript from "rollup-plugin-typescript2";
+import { terser } from "rollup-plugin-terser";
+
+const isProduction = process.env.NODE_ENV === "production";
+
+export default {
+  input: "src/index.tsx",
+  output: {
+    file: "build/js/index.min.js",
+    format: "iife",
+    sourcemap: !isProduction
+  },
+  plugins: [
+    resolve(),
+    commonjs(),
+    typescript({
+      typescript: require("typescript")
+    }),
+    isProduction && terser()
+  ]
+};
