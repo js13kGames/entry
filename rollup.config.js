a8ad98b060cd8a824c8a141a2c26ded1c0ecdd82
Fawwaz <fawwazmuhammad@gmail.com>
2019-09-13 08:15:24 +0700

    remove linaria

--- rollup.config.js
+++ rollup.config.js
@@ -2,8 +2,6 @@ import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
 import { terser } from "rollup-plugin-terser";
-import linaria from 'linaria/rollup';
-import css from 'rollup-plugin-css-only';
 
 const isProduction = process.env.NODE_ENV === "production";
 console.log(process.env.NODE_ENV)
@@ -20,12 +18,6 @@ export default {
     commonjs(),
     typescript({
       typescript: require("typescript")
-    }),
-    linaria({
-      sourceMap: !isProduction
-    }),
-    css({
-      output: (isProduction ? "build/styles.css" : "src/styles.css"),
     }),
     isProduction && terser()
   ]
